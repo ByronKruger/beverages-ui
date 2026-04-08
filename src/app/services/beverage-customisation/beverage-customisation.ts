851857789964cedbf1c19f31d8d13f172e8e2211
ByronKruger <byron@synerics.com>
2026-04-08 18:40:05 +0200

    Added usage of env baseUrl value in ng service

--- src/app/services/beverage-customisation/beverage-customisation.ts
+++ src/app/services/beverage-customisation/beverage-customisation.ts
@@ -1,15 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BeverageCustomisation {
-
   private httpClient = inject(HttpClient);
+  private baseUrl = environment.apiBaseUrl;
 
   public searchUserByName(): any {
-    this.httpClient.get(`/api/beverage-customisation/users?name=${"Alessio001@gmail.com"}`).subscribe((response) => {
+    this.httpClient.get(`${this.baseUrl}/api/beverage-customisation/users?name=${"Alessio001@gmail.com"}`).subscribe((response) => {
       console.log(response);
     });
   }
