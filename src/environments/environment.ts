77a195ecdc6b16c6884858fd74e4c257048455ce
ByronKruger <byron@synerics.com>
2026-06-15 15:31:46 +0200

    change suffix for call to functions app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     // apiBaseUrl: 'https://beverages-api.azurewebsites.net',
-    apiBaseUrl: 'https://myfuncapp-ezb9h9csccdtgsc0.southafricanorth-01.azurewebsites.net/api/api',
+    apiBaseUrl: 'https://myfuncapp-ezb9h9csccdtgsc0.southafricanorth-01.azurewebsites.net/',
     production: true
 };
