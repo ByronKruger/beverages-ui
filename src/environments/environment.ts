94305f08d8120e1809ccea9341fec7fa6665eaea
ByronKruger <byron@synerics.com>
2026-06-17 19:11:20 +0200

    Added necessary /api/ prefix for remote swa'

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     // apiBaseUrl: 'https://beverages-api.azurewebsites.net',
-    apiBaseUrl: '',
+    apiBaseUrl: '/api/',
     // apiBaseUrl: 'https://myfuncapp-ezb9h9csccdtgsc0.southafricanorth-01.azurewebsites.net/api/',
     production: true
 };
