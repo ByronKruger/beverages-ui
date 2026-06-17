0da634dfccd526511d54977cef607765e3816329
ByronKruger <byron@synerics.com>
2026-06-17 18:57:09 +0200

    Change endpoint to epty string due to functions app configure for swa (cors)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     // apiBaseUrl: 'https://beverages-api.azurewebsites.net',
-    apiBaseUrl: 'https://myfuncapp-ezb9h9csccdtgsc0.southafricanorth-01.azurewebsites.net/api/',
+    apiBaseUrl: '',
+    // apiBaseUrl: 'https://myfuncapp-ezb9h9csccdtgsc0.southafricanorth-01.azurewebsites.net/api/',
     production: true
 };
